cee8b0ac777d7f63ebe2a21ca745ef3d15bfe7f1
ahmed230104098 <ahmedemadr2005@gmail.com>
2025-04-04 20:25:38 +0200

    .

--- app/Models/Grade.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Grade extends Model {
-    protected $fillable = ['course_name', 'course_code', 'credit_hours', 'letter_grade', 'grade_point', 'term'];
-}
--- resources/views/exam/result.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-@section('content')
-    <h2>Exam Result</h2>
-    <p>Your score: {{ $score }} / {{ $total }}</p>
-@endsection
